3f496a387ddc5641f53b76697a1804ac61cf875a
YFshadaow <renzd2001@gmail.com>
2023-09-10 13:42:28 +0800

    Update to 1.1.0:   - Removed dependency plugman   - Removed periodical notification due to SpigotAPI 's restrictions   - Removed github workflow   - Clean code

--- src/main/java/fun/kaituo/AutoDeploy.java
+++ src/main/java/fun/kaituo/AutoDeploy.java
@@ -1,6 +1,5 @@
 package fun.kaituo;
 
-import com.rylinaux.plugman.event.PluginLoadEvent;
 import org.apache.ftpserver.DataConnectionConfiguration;
 import org.apache.ftpserver.DataConnectionConfigurationFactory;
 import org.apache.ftpserver.FtpServer;
@@ -14,11 +13,8 @@ import org.apache.ftpserver.usermanager.impl.WritePermission;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
@@ -26,36 +22,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.*;
 
-import static org.bukkit.ChatColor.AQUA;
-import static org.bukkit.ChatColor.RED;
-
 public class AutoDeploy extends JavaPlugin implements Listener {
-    int port;
-    int passivePortsStart;
-    int passivePortsEnd;
-    String passiveExternalAddress;
-    FtpServer server;
-    Set<String> fileNames;
-    Set<String> fileNamesBuffer;
+    private FtpServer server;
+    private final Set<String> pluginNamesBuffer = new HashSet<>();
 
+    public Set<String> getPluginNamesBuffer() {
+        return pluginNamesBuffer;
+    }
+    
     @Override
     public void onEnable() {
         saveDefaultConfig();
         FileConfiguration c = getConfig();
-        port = c.getInt("bind-port");
-        passiveExternalAddress = c.getString("passive-external-address");
-        passivePortsStart = c.getInt("passive-ports-start");
-        passivePortsEnd = c.getInt("passive-ports-end");
-        fileNames = new HashSet<>();
-        fileNamesBuffer = new HashSet<>();
+        int port = c.getInt("bind-port");
+        String passiveExternalAddress = c.getString("passive-external-address");
+        int passivePortsStart = c.getInt("passive-ports-start");
+        int passivePortsEnd = c.getInt("passive-ports-end");
+        String homeDirectory = c.getString("home-directory");
         Bukkit.getPluginManager().registerEvents(this, this);
-        getCommand("adclear").setExecutor(this);
         try {
-            DataConnectionConfigurationFactory dataConnectionConfigurationFactory = new DataConnectionConfigurationFactory();
-            dataConnectionConfigurationFactory.setPassivePorts(passivePortsStart + "-" + passivePortsEnd);
-            dataConnectionConfigurationFactory.setPassiveExternalAddress(passiveExternalAddress);
-            dataConnectionConfigurationFactory.setActiveEnabled(false);
-            DataConnectionConfiguration dataConnectionConfiguration = dataConnectionConfigurationFactory.createDataConnectionConfiguration();
+            DataConnectionConfiguration dataConnectionConfiguration = getDataConnectionConfiguration(passivePortsStart, passivePortsEnd, passiveExternalAddress);
             FtpServerFactory serverFactory = new FtpServerFactory();
             ListenerFactory listenerFactory = new ListenerFactory();
             listenerFactory.setPort(port);
@@ -66,7 +52,7 @@ public class AutoDeploy extends JavaPlugin implements Listener {
             BaseUser user = new BaseUser();
             user.setName(c.getString("username"));
             user.setPassword(c.getString("password"));
-            user.setHomeDirectory("/home/yfshadaow/servers/minigame/plugins/");
+            user.setHomeDirectory(homeDirectory);
             List<Authority> authorities = new ArrayList<>();
             authorities.add(new WritePermission());
             user.setAuthorities(authorities);
@@ -80,31 +66,26 @@ public class AutoDeploy extends JavaPlugin implements Listener {
         } catch (FtpException e) {
             e.printStackTrace();
         }
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            StringBuilder msg = new StringBuilder("§b以下插件需要加载/重载： ");
-            if (fileNames.isEmpty()) {
-                return;
-            } else for (String fileName : fileNames) {
-                msg.append("§f").append(fileName).append(", ");
-            }
-            msg.delete(msg.length() - 2, msg.length() - 1);
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                if (p.hasPermission("autodeploy.notify")) {
-                    p.sendMessage(msg.toString());
-                }
-            }
-        }, 3600, 3600);
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, ()-> {
-            for (String name: fileNamesBuffer) {
+            for (String name: pluginNamesBuffer) {
                 for (Player p : Bukkit.getOnlinePlayers()) {
                     if (p.hasPermission("autodeploy.notify")) {
                         p.sendMessage("§f" + name + "§b需要加载/重载！");
                         p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, SoundCategory.MASTER, 1, 1);
                     }
                 }
+                getLogger().info("§f" + name + "§b需要加载/重载！");
             }
-            fileNamesBuffer.clear();
+            pluginNamesBuffer.clear();
         },1,1);
+    }
+
+    private DataConnectionConfiguration getDataConnectionConfiguration(int passivePortsStart, int passivePortsEnd, String passiveExternalAddress) {
+        DataConnectionConfigurationFactory dataConnectionConfigurationFactory = new DataConnectionConfigurationFactory();
+        dataConnectionConfigurationFactory.setPassivePorts(passivePortsStart + "-" + passivePortsEnd);
+        dataConnectionConfigurationFactory.setPassiveExternalAddress(passiveExternalAddress);
+        dataConnectionConfigurationFactory.setActiveEnabled(false);
+        return dataConnectionConfigurationFactory.createDataConnectionConfiguration();
     }
 
     @Override
@@ -114,25 +95,4 @@ public class AutoDeploy extends JavaPlugin implements Listener {
         HandlerList.unregisterAll((Plugin) this);
     }
 
-    @EventHandler
-    public void onPluginLoad(PluginLoadEvent event) {
-        fileNames.remove(event.getFileName());
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("adclear")) {
-            if (sender instanceof Player) {
-                if (sender.isOp()) {
-                    fileNames.clear();
-                    sender.sendMessage(AQUA + "已清空未处理插件列表");
-                } else {
-                    sender.sendMessage(RED + "你没有权限执行这个指令！");
-                }
-            } else {
-                sender.sendMessage(RED + "此指令只能由玩家执行！");
-            }
-        }
-        return true;
-    }
 }
--- src/main/java/fun/kaituo/PluginFtplet.java
+++ src/main/java/fun/kaituo/PluginFtplet.java
@@ -2,41 +2,29 @@ package fun.kaituo;
 
 import org.apache.ftpserver.ftplet.*;
 
-import java.util.Set;
-
 public class PluginFtplet extends DefaultFtplet {
-    Set<String> fileNames;
-    Set<String> fileNameBuffer;
+    private final AutoDeploy plugin;
 
     public PluginFtplet(AutoDeploy plugin) {
-        this.fileNames = plugin.fileNames;
-        this.fileNameBuffer = plugin.fileNamesBuffer;
+        this.plugin = plugin;
 
     }
 
     @Override
     public void init(FtpletContext context) {
-        System.out.println("Ftplet initializing");
+        plugin.getLogger().info("Ftplet initializing");
     }
 
     @Override
     public void destroy() {
-        System.out.println("Ftplet destroying");
+        plugin.getLogger().info("Ftplet destroying");
     }
 
-    /*
-    @Override
-    public FtpletResult onUploadStart(FtpSession session, FtpRequest request) {
-        System.out.println("yeah!");
-        return FtpletResult.DEFAULT;
-    }
-
-     */
     @Override
     public FtpletResult onUploadEnd(FtpSession session, FtpRequest request) {
         String fileName = request.getArgument();
-        fileNames.add(fileName);
-        fileNameBuffer.add(fileName);
+
+        plugin.getPluginNamesBuffer().add(fileName);
         return FtpletResult.DEFAULT;
     }
 }
